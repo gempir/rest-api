9685bca6a98696149e8bf36e3dbd17f35056a2e0
gempir <daniel.pasch.s@gmail.com>
2017-10-20 20:06:37 +0200

    init

---
+++ src/Router.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace gempir\api;
+
+class Router
+{
+
+}
---
+++ tests/RouterTest.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace gempir\api;
+
+use PHPUnit\Framework\TestCase;
+
+class RouterTest extends TestCase
+{
+
+}
